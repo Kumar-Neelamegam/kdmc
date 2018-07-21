7c7578d87b66c204e03441d6561a7e56ffee0021
Android <muthukumar.n@devatechinfosystems.com>
2018-07-21 11:55:08 +0530

    21-07-2018

--- app/src/main/java/kdmc_kumar/Core_Modules/MyPatient.java
+++ app/src/main/java/kdmc_kumar/Core_Modules/MyPatient.java
@@ -961,17 +961,6 @@ public class MyPatient extends AppCompatActivity implements TextWatcher {
             c.close();
             db.close();
 
-/*
-            SqliteReader.getInstance().setQuery(LOAD_ALL_PATIENTS_QUERY).onDataReceiver(new SqliteReader.Data() {
-                @Override
-                public void onData(Cursor c) {
-
-
-
-
-                }
-            });*/
-
         } catch (Exception e) {
             e.printStackTrace();
         }
